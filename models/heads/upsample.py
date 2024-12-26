e9b08ab1f2217af640b0f5af34a771d8ee3502e4
Arseniy <arsenator337@gmail.com>
2024-12-27 02:53:27 +0300

    dev

---
+++ models/heads/upsample.py
@@ -0,0 +1,33 @@
+import torch
+import torch.nn as nn
+
+
+class Upsample(nn.Module):
+    def __init__(self, in_channels, num_labels):
+        super().__init__()
+
+        self.in_channels = in_channels
+        self.num_labels = num_labels
+
+        self.model = nn.ConvTranspose2d(
+            in_channels,
+            num_labels,
+            3,
+        )
+
+    def forward(self, embeddings):
+        out = self.model(
+            embeddings, 
+            output_size=torch.Size([
+                embeddings.shape[0], 
+                self.num_labels,
+                224,
+                224
+            ])
+        )
+        # out = nn.functional.interpolate(out, size=(224, 224), mode="bilinear", align_corners=False)
+        return out
+    
+
+    def get_name(self):
+        return "Linear Classifier"
