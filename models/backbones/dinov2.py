1d0236da483f8444664a81d5f0319b6f71a30ca3
Arseniy <arsenator337@gmail.com>
2024-12-27 01:54:45 +0300

    dev

--- models/backbones/dinov2.py
+++ models/backbones/dinov2.py
@@ -2,10 +2,26 @@ import torch
 import torch.nn as nn
 
 
+VERSION_TO_CHANNELS = {
+    "dinov2_vits14_reg": 384 * 4,
+    "dinov2_vitb14_reg": 768 * 4,
+    "dinov2_vitl14_reg": 1024 * 4,
+    "dinov2_vitg14_reg": 1536 * 4,
+}
+
+VERSION_TO_LAYERS = {
+    "dinov2_vits14_reg": [8, 9, 10, 11],
+    "dinov2_vitb14_reg": [8, 9, 10, 11],
+    "dinov2_vitl14_reg": [20, 21, 22, 23],
+    "dinov2_vitg14_reg": [36, 37, 38, 39],
+}
+
+
 class DinoV2(nn.Module):
-    def __init__(self, freeze, reshape):
+    def __init__(self, freeze, reshape, version):
         super().__init__()
-        self.model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitl14_reg').cuda()
+        self.version = f"dinov2_vit{version}14_reg"
+        self.model = torch.hub.load('facebookresearch/dinov2', self.version).cuda()
 
         self.reshape = reshape
         self.freeze = freeze
@@ -15,9 +31,16 @@ class DinoV2(nn.Module):
                 param.requires_grad = False
 
     def forward(self, images):
-        out = self.model.get_intermediate_layers(images, [20, 21, 22, 23], reshape=self.reshape)
+        out = self.model.get_intermediate_layers(
+            images,
+            VERSION_TO_LAYERS[self.version],
+            reshape=self.reshape
+        )
         out = torch.cat(out, dim=1)
         return out
     
+    def get_out_channels(self):
+        return VERSION_TO_CHANNELS[self.version]
+
     def get_name(self):
         return f"DinoV2 {'(frozen)' if self.freeze else ''}"
--- models/segmentation.py
+++ models/segmentation.py
@@ -8,7 +8,10 @@ class Segmentation(nn.Module):
         super().__init__()
         
         self.backbone = instantiate(backbone_conf)
-        self.head = instantiate(head_conf)
+        self.head = instantiate(
+            head_conf,
+            in_channels=self.backbone.get_out_channels()
+        )
 
     def forward(self, images):
         out = self.backbone(images)
