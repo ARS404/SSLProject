abc8ad5842ffd31c61353f59b201de273c1b24b3
Arseniy <arsenator337@gmail.com>
2024-12-26 18:26:20 +0300

    dev

--- utils/utils_vis.py
+++ utils/utils_vis.py
@@ -40,7 +40,7 @@ def visualize_map(image, segmentation_map):
         color_seg[segmentation_map == label, :] = color
 
     # Show image + mask
-    color_seg.permute(0, 3, 1, 2)
+    color_seg.transpose(0, 3, 1, 2)
     img = np.array(image) * 0.5 + color_seg * 0.5
     img = img.astype(np.uint8)      # [batch, channels, h, w]
 
