e97e40334f5ac57b279ceeddab11209e45cb9f00
Arseniy <arsenator337@gmail.com>
2024-12-26 21:57:10 +0300

    dev

--- train/metrics.py
+++ train/metrics.py
@@ -3,7 +3,7 @@ from torchmetrics import JaccardIndex
 class MIOU(object):
     def __init__(self, num_classes):
         self.num_classes = num_classes
-        self.jaccard = JaccardIndex(task='multiclass', num_classes=21, ignore_index=0).cuda()
+        self.jaccard = JaccardIndex(task='multiclass', num_classes=num_classes, ignore_index=0).cuda()
 
     def __call__(self, logits, labels):
         return self.jaccard(logits, labels)
