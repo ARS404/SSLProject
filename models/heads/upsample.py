c37cb4c80811e8678aee5ac9fabcf5716d08713c
Arseniy <arsenator337@gmail.com>
2024-12-27 04:01:45 +0300

    dev

--- models/heads/upsample.py
+++ models/heads/upsample.py
@@ -13,10 +13,22 @@ class Upsample(nn.Module):
         self.upconv1 = nn.ConvTranspose2d(
             in_channels,
             self.hidden_dim,
-            kernel_size=8,
-            stride=4
+            kernel_size=4,
+            stride=2
         )
         self.upconv2 = nn.ConvTranspose2d(
+            self.hidden_dim,
+            self.hidden_dim,
+            kernel_size=6,
+            stride=3
+        )
+        self.upconv3 = nn.ConvTranspose2d(
+            self.hidden_dim,
+            self.hidden_dim,
+            kernel_size=6,
+            stride=3
+        )
+        self.upconv4 = nn.ConvTranspose2d(
             self.hidden_dim,
             self.num_labels,
             kernel_size=8,
@@ -25,8 +37,12 @@ class Upsample(nn.Module):
 
     def forward(self, embeddings):
         out = self.upconv1(embeddings)
-        out = nn.functional.interpolate(out, size=(112, 112), mode="bilinear", align_corners=False)
+        out = nn.functional.interpolate(out, size=(28, 28), mode="bilinear", align_corners=False)
         out = self.upconv2(out)
+        out = nn.functional.interpolate(out, size=(56, 56), mode="bilinear", align_corners=False)
+        out = self.upconv3(out)
+        out = nn.functional.interpolate(out, size=(112, 112), mode="bilinear", align_corners=False)
+        out = self.upconv4(out)
         out = nn.functional.interpolate(out, size=(224, 224), mode="bilinear", align_corners=False)
         return out
     
