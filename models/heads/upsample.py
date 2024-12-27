2dceaadf3997a61a32eb8b56db71d434c617a79e
Arseniy <arsenator337@gmail.com>
2024-12-27 03:15:46 +0300

    dev

--- models/heads/upsample.py
+++ models/heads/upsample.py
@@ -3,22 +3,50 @@ import torch.nn as nn
 
 
 class Upsample(nn.Module):
-    def __init__(self, in_channels, num_labels):
+    def __init__(self, in_channels, num_labels, hidden_dim):
         super().__init__()
 
+        self.hidden_dim = hidden_dim
         self.in_channels = in_channels
         self.num_labels = num_labels
 
-        self.model = nn.ConvTranspose2d(
-            in_channels,
-            num_labels,
-            kernel_size=26,
-            stride=13
+        self.model = nn.Sequential([
+            nn.ConvTranspose2d( # 16
+                in_channels,
+                self.hidden_dim,
+                kernel_size=4,
+                stride=2
+            ),
+            nn.ConvTranspose2d( #32
+                self.hidden_dim,
+                self.hidden_dim,
+                kernel_size=4,
+                stride=2
+            ),
+            nn.ConvTranspose2d( #64
+                self.hidden_dim,
+                self.hidden_dim,
+                kernel_size=4,
+                stride=2
+            ),
+            nn.ConvTranspose2d( #128
+                self.hidden_dim,
+                self.hidden_dim,
+                kernel_size=4,
+                stride=2
+            ),
+        ])
+        self.final = nn.ConvTranspose2d(
+            self.hidden_dim,
+            self.num_labels,
+            kernel_size=4,
+            stride=2
         )
 
     def forward(self, embeddings):
-        out = self.model(
-            embeddings, 
+        out = self.model(embeddings)
+        out = self.final(
+            out,
             output_size=torch.Size([
                 embeddings.shape[0], 
                 self.num_labels,
@@ -26,7 +54,6 @@ class Upsample(nn.Module):
                 224
             ])
         )
-        # out = nn.functional.interpolate(out, size=(224, 224), mode="bilinear", align_corners=False)
         return out
     
 
